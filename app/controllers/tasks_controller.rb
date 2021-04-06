4a7ea8ff14e58e4d44abb781f36a3670f4a9f8fa
soki.hideshima <ssookki0728@au.com>
2021-04-06 14:54:39 +0000

    kadai-task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -45,12 +45,12 @@ class TasksController < ApplicationController
 
     flash[:success] = 'タスクは正常に削除されました'
     redirect_to tasks_url
+  end
     
      private
 
   # Strong Parameter
-  def message_params
-    params.require(:message).permit(:content)
-  end
+  def task_params
+    params.require(:task).permit(:content)
   end
 end  
